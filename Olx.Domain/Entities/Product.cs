02b00e008a2ea0f931b0c2f0a2b6e4600be20005
bekzod28072009 <bekhzodkeldiyorov@gmail.com>
2024-01-16 22:01:11 +0500

    Domain

---
+++ Olx.Domain/Entities/Product.cs
@@ -0,0 +1,21 @@
+﻿using Olx.Domain.Common;
+using Olx.Domain.Enums;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Olx.Domain.Entities
+{
+    public class Product : Auditable
+    {
+        public string Name { get; set; }
+
+        public int Price { get; set; }
+
+        public Category Category { get; set; }
+
+        public string Inormation { get; set; }
+    }
+}
---
+++ Olx.Domain/Enums/Category.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Olx.Domain.Enums
+{
+    public enum Category
+    {
+        Televizor,
+        Telefon,
+        Komputer,
+        Xolodilni
+    }
+}
